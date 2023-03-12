ca319d57db3ab9f73f3ead0cf2222d6a8c491474
Dima <d1ctators74@yandex.ru>
2023-03-12 11:26:43 +0300

    first commit

---
+++ js/sliders.js
@@ -0,0 +1,26 @@
+
+
+// const swiper = new Swiper('.slider-top-films', {
+// 	lidesPerView: "auto",
+// 	centeredSlides: true,
+// 	watchOverflow: true,
+// 	watchSlidesProgress: true,
+// 	updateOnWindowResize: true,
+// 	initialSlide: 1,
+// 	centeredSlides: true,
+// 	loopAdditionalSlides: 100,
+// 	slidesPerView: "auto",
+// 	loop: true,
+// 	loopedSlides: 8,
+// 	spaceBetween: 30,
+// 	autoplay: {
+// 		delay: 15000,
+// 		disableOnInteraction: false
+// 	},
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+
+// });
+
